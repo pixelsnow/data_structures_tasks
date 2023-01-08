bb29f7a5bb8d0166eeac367b8fc07c7bd53d2a9e
Vagapova Valeriia <lera.vagapova@gmail.com>
2023-01-08 23:57:11 +0200

    add working palindrome

--- src/Task1.jsx
+++ src/Task1.jsx
@@ -3,19 +3,29 @@ import React, { useState } from "react";
 import Result from "./Task1/Result";
 
 const Task1 = () => {
-  const { text, setText } = useState("");
+  const [text, setText] = useState("");
 
   const handleInputChange = (e) => {
     setText(e.target.value);
+    console.log(text);
   };
 
-  const isPalindrome = (string) => {};
+  const isPalindrome = (string) => {
+    for (let i = 0; i < Math.floor(string.length / 2); i++) {
+      console.log("len = ", string.length, ", i = ", i);
+      if (string[i] !== string[string.length - i - 1]) {
+        console.log(string[i], "=/=", string[string.length - i - 1]);
+        return false;
+      }
+    }
+    return true;
+  };
 
   return (
     <div>
       <h1>Task1</h1>
       <input type="text" onChange={handleInputChange} />
-      <Result isPalindrome={false} />
+      <Result isPalindrome={isPalindrome(text)} />
     </div>
   );
 };
